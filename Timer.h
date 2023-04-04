d33c6dcd6452c1341c2f2b9fa077d478d00cb12b
Olive251 <oliviataylor437@gmail.com>
2023-04-04 10:47:07 -0400

    added timer class for demo

---
+++ Timer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <time.h>
+#define POSIX 1
+#define WINDOWS 2
+
+#define PLATFORM WINDOWS //POSIX
+
+class Timer {
+#if PLATFORM == WINDOWS
+    clock_t starttime_;
+    clock_t stoptime_;
+#else
+    struct timespec starttime_;
+    struct timespec stoptime_;
+#endif
+    double elapsedTime_;
+    bool isstop_;
+public:
+    Timer();
+    void reset();
+    void start();
+    void stop();
+    double currtime();
+    double starttime();
+};
