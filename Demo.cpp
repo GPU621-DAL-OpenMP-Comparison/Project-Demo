fe48238c5c1ed2885ccb75f22b3b14512c15d0f7
Olive251 <oliviataylor437@gmail.com>
2023-04-06 13:41:23 -0400

    completed img display

--- Demo.cpp
+++ Demo.cpp
@@ -4,12 +4,17 @@
 //TODO accept CL arguments
 int main() {
 
-	Tester demo("../sample_images/field.jpg");
+	//Tester demo("../sample_images/field.jpg");
+	//D:\School_Win23\GPU625\Project\Project-Demo\Project-Demo\sample_images
+	//Tester demo("D:/School_Win23/GPU625/Project/Project-Demo/Project-Demo/sample_images/field.jpg");
+	
+	Tester demo("c:/Users/Olivia/Desktop/sample_images/field.jpg");
 	//show starter images
 	//do omp run
 	//show times and altered images
 	//do DAAL run
 	//show times and altered images
+	demo.display_img();
 
 	demo.omp_brighten(50);
 	demo.omp_sharpen();
