f1fcbf49f5d352ec154411dd28e06b44e3502b04
Olive251 <oliviataylor437@gmail.com>
2023-04-12 13:05:04 -0400

    slight imprv omp_sharpen runtime w/ reduction

--- openMP_imgProc.cpp
+++ openMP_imgProc.cpp
@@ -11,19 +11,22 @@ void openMP_imgProcessor::sharpenImg(cv::Mat& image) {
     cv::Mat grayscale;
     cv::cvtColor(image, grayscale, cv::COLOR_BGR2GRAY);
     
-    #pragma omp parallel for    
+    double sum = 0.0;
+    #pragma omp parallel for reduction(+:sum)
     for (int x = 1; x < image.cols - 1; x++) {
         for (int y = 1; y < image.rows - 1; y++) {
-            double sum = 0.0;
+            double local_sum = 0.0;
             for (int i = -1; i <= 1; i++) {
                 for (int j = -1; j <= 1; j++) {
-                    sum += grayscale.at<uchar>(y + j, x + i) * LapKernel_[i + 1][j + 1];
+                    local_sum += grayscale.at<uchar>(y + j, x + i) * LapKernel_[i + 1][j + 1];
                 }
             }
-            
+
             for (int c = 0; c < 3; c++) {
-                image.at<cv::Vec3b>(y, x)[c] = cv::saturate_cast<uchar>(image.at<cv::Vec3b>(y, x)[c] + sum * .99);
+                image.at<cv::Vec3b>(y, x)[c] = cv::saturate_cast<uchar>(image.at<cv::Vec3b>(y, x)[c] + local_sum * .99);
             }
+
+            sum += local_sum;
         }
     }
 
